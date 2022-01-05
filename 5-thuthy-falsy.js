e44a08e0228504ad4cb807463a3f3870d7f2bbe9
paola <paolapoffo1@gmail.com>
2022-01-05 17:19:50 -0300

    Exemplos aula 2 - declarando variáveis

---
+++ 5-thuthy-falsy.js
@@ -0,0 +1,34 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy of falsy
+
+// 0  => false
+// "" => false
+// 1  => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+//undefined 
+//null - vazio
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(null);
+
+console.log(typeof minhaVar);
+console.log(typeof null);
+
+let numero = 3;
+let texto = ("Alura");
+
+//typeof = perguntar qual o tipo de dado que está sendo gurdado
+
+console.log(typeof numero);
+console.log(typeof texto);
---
+++ 6-conversao-de-tipos.js
@@ -0,0 +1,23 @@
+// tipo de dado 
+// booleanos 
+
+//conversão implícita 
+
+const numero = 456;
+const numeroString = ("456");
+
+//=== leva em consideração o tipo de dado
+console.log(numero === numeroString);
+
+//== não leva em consideração o tipo de dado (apenas valor)
+console.log(numero == numeroString);
+
+//concatenação
+console.log(numero + numeroString);
+
+//Number() - Conversor
+//String() - Conversor
+
+//conversão explícita 
+console.log(numero + Number(numeroString));
+console.log(numero === Number(numeroString));
